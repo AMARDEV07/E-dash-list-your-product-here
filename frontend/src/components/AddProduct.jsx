98b2e71e84c2bbb3e7e9d3de613b9378513b8ec5
Amar dev panwar <127836087+AMARDEV07@users.noreply.github.com>
2025-05-08 13:34:03 +0530

    Update AddProduct.jsx

--- frontend/src/components/AddProduct.jsx
+++ frontend/src/components/AddProduct.jsx
@@ -49,7 +49,7 @@ function AddProduct() {
 
         // Backend ko POST request bhej rahe hain product details ke saath
 
-        const result = await fetch("http://localhost:3000/add-product", {
+        const result = await fetch("https://e-dash-list-your-product-here-2.onrender.com/add-product", {
             method: "POST",
             body: JSON.stringify({
                 name,
